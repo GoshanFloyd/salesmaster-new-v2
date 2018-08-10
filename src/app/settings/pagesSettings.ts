770aabb06f306d8f1f176e758df3e04ce1bd6fed
George Kulik <egorkulik9696@gmail.com>
2018-08-10 15:48:36 +0600

    work on pagesSettings

--- src/app/settings/pagesSettings.ts
+++ src/app/settings/pagesSettings.ts
@@ -1,36 +1,36 @@
 
 export const pagesSettings = {
   'contacts' : {
-
+    enable: true
   },
   'profile': {
-
+    enable: true
   },
   'desktop': {
-
+    enable: true
   },
   'tasks': {
-
+    enable: true
   },
   'analytics': {
-
+    enable: true
   },
   'file-manager': {
-
+    enable: true
   },
   'products': {
-
+    enable: false
   },
   'reports': {
-
+    enable: true
   },
   'handbook': {
-
+    enable: true
   },
   'help': {
-
+    enable: true
   },
   'site-request': {
-
+    enable: false
   }
 };
